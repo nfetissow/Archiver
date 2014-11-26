818290f17bf16a0456ce66bc60f62a13ac35a528
NFetissow <nfetissow@gmail.com>
2014-11-26 16:40:07 +0300

    Added test for Archiving

--- src/test/java/ArchiverTest.java
+++ src/test/java/ArchiverTest.java
@@ -6,23 +6,22 @@ import ru.ncedu.nfetissow.archiver.ArchiverApp;
 import ru.ncedu.nfetissow.archiver.ArchiverImpl;
 
 public class ArchiverTest {
-
-
     @Test
     public void testDearchiving() {
         Archiver arch = ArchiverImpl.getInstance();
         String actual = arch.deArchive("/test/testResources/toUnZip/main.c.zip", "/test/dearchiveResult");
         Assert.assertEquals(actual, "Success");
-        actual = arch.deArchive("/test/testResources/toUnZip/HelloWorld.zip", "/test/dearchiveResult");
+        actual = arch.deArchive("/test/testResources/toUnZip/HelloWorld.zip", "/test/testResources/dearchiveResult");
         Assert.assertEquals(actual, "Success");
         actual = arch.deArchive("notExisting.zip", "/test/dearchiveResult");
         Assert.assertEquals(actual, "Error");
     }
-
     @Test
     public void testArchiving() {
-
+        Archiver arch = ArchiverImpl.getInstance();
+        String actual = arch.createArchive("/test/testResources/ToZip/folder", "/test/testResources/folder.zip");
+        Assert.assertEquals(actual, "Success");
+        actual = arch.createArchive("/test/testResources/ToZip/main", "/test/testResources/main.zip");
+        Assert.assertEquals(actual, "Success");
     }
-
-
 }
